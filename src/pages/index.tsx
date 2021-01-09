1b703c110feca68783808f562acba48da99174d8
blackhatplay <mannuall4u@gmail.com>
2021-01-09 23:54:03 +0530

    tittle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import Main from '../components/MainContent';
 import Header from "../components/Header";
+import Head from 'next/head';
+
 
 const index = () => {
   return (
     <>
+      <Head>
+        <title>Todo</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Header />
       <Main />
     </>
